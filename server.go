d548c5320478b34ae072c660c855b4f0beb078f2
masafumi330 <mihusamawasami@gmail.com>
2023-12-17 12:23:02 +0900

    :zap: recover

--- server.go
+++ server.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Recover())
 	e.GET("/", func(c echo.Context) error {
+		panic("panic")
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
